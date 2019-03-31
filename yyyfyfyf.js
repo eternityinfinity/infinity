d94c57bd954c9206804f53bed078f1cd7eb7fe60
eternityinfinity <46814554+eternityinfinity@users.noreply.github.com>
2019-04-01 01:00:55 +0300

    Create yyyfyfyf.js

---
+++ yyyfyfyf.js
@@ -0,0 +1,4 @@
+  function youTube(a,b,c,d)
+{
+  return ("<iframe allowfullscreen='1' frameborder='0' height='140' src='https://www.youtube.com/embed/"+a+"' width='210'/><iframe allowfullscreen='1' frameborder='0' height='140' src='https://www.youtube.com/embed/"+b+"' width='210'/><iframe allowfullscreen='1' frameborder='0' height='140' src='https://www.youtube.com/embed/"+c+"' width='210'/><iframe allowfullscreen='1' frameborder='0' height='140' src='https://www.youtube.com/embed/"+d+"' width='210'/>");
+}
